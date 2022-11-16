aa6685b22e8744c06c183466e6d2452fd54028cb
alpacaHyun <flaehdan@gmail.com>
2022-11-17 08:06:36 +0900

    type : feat 프로그래머스
    body :

--- src/programmers/level_1/divideNum.java
+++ src/programmers/level_1/divideNum.java
@@ -8,18 +8,6 @@ public class divideNum {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String a = br.readLine();
-        String[] b =
-
-        int[] arr =
-
-        int[] answer = {};
-
-
-
-
-        return answer;
 
     }
 
