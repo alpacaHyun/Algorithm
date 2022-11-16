cf821b31be61c42f653600de2b0eb3c3e600a547
alpacaHyun <flaehdan@gmail.com>
2022-11-16 16:48:58 +0900

    알고리즘

---
+++ src/programmers/level_1/divideNum.java
@@ -0,0 +1,26 @@
+package programmers.level_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class divideNum {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String[] b =
+
+        int[] arr =
+
+        int[] answer = {};
+
+
+
+
+        return answer;
+
+    }
+
+}
