04283ed17750f27f9f01e2aecac1953fb13a369f
alpacaHyun <flaehdan@gmail.com>
2022-11-18 14:50:35 +0900

    type : feat 백준 브론즈 4
    body :

---
+++ src/baekjoon/bronze/Bronze3046.java
@@ -0,0 +1,23 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bronze3046 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+
+        String n = br.readLine();
+        String[] a = n.split(" ");
+
+        int R1 = Integer.parseInt(a[0]);
+        int S = Integer.parseInt(a[1]);
+
+        int R2 = (S * 2) - R1;
+        System.out.println(R2);
+
+    }
+}
