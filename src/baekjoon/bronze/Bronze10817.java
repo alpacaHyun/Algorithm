0cdd35c29375a50d4bed4be46ab9a8d0be6c2808
alpacaHyun <flaehdan@gmail.com>
2022-11-20 09:38:33 +0900

    type : feat 백준 브론즈 3
    body :

---
+++ src/baekjoon/bronze/Bronze10817.java
@@ -0,0 +1,28 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Bronze10817 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        String[] inputNum = input.split(" ");
+        List<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            nums.add(Integer.parseInt(inputNum[i]));
+        }
+        Collections.sort(nums);
+
+        System.out.println(nums.get(1));
+    }
+}
