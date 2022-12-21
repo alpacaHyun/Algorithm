8369287ac8f4b50dea6dbf26f1f4e596f9033070
alpacaHyun <flaehdan@gmail.com>
2022-12-21 22:52:33 +0900

    feat : 완성
    body :

--- src/programmers/level_1/FruitSeller.java
+++ src/programmers/level_1/FruitSeller.java
@@ -2,35 +2,34 @@ package programmers.level_1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class FruitSeller {
 
     public static void main(String[] args) {
-        int[] score = {5, 4, 3, 2, 1};
+        int[] score = {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2};
 
-        System.out.println(solution(1, 2, score));
+        System.out.println(solution(4, 3, score));
     }
 
     public static int solution(int k, int m, int[] score) {
 
         int answer = 0;
-        int boxCount = (score.length / m);
         List<int[]> boxList = new ArrayList<>();
         int boxEnd = score.length;
 
         Arrays.sort(score);
 
-        while(boxEnd > boxCount) {
-
-            boxList.add(Arrays.copyOfRange(score, (boxEnd - boxCount), boxEnd));
-
-            boxEnd -= boxCount;
+        while(boxEnd - m >= 0) {
+            boxList.add(Arrays.copyOfRange(score, (boxEnd - m), boxEnd));
+            boxEnd -= m;
         }
 
-        return 0;
+        for (int i = 0; i < boxList.size(); i++) {
+            int[] tmp = boxList.get(i);
+            answer += tmp[0] * m;
+        }
+
+        return answer;
     }
 }
