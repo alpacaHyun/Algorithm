a38644826e06123415d819dcd188ea41fcd70d1e
alpacaHyun <flaehdan@gmail.com>
2022-12-26 22:05:03 +0900

    feat : 프로그래머스 Lv1
    body :

---
+++ src/programmers/level_1/AddNumber.java
@@ -0,0 +1,19 @@
+package programmers.level_1;
+
+public class AddNumber {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        String tmp = String.valueOf(n);
+
+        for (int i = 0; i < tmp.length(); i++) {
+
+            answer += Integer.parseInt(String.valueOf(tmp.charAt(i)));
+
+        }
+
+        return answer;
+    }
+
+}
