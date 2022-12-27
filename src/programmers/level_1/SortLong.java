e3d301bb6c5b7a183f4d4fcf7d9725ea97e1a647
alpacaHyun <flaehdan@gmail.com>
2022-12-27 22:41:45 +0900

    feat : 프로그래머스 Lv1
    body :

--- src/programmers/level_1/SortLong.java
+++ src/programmers/level_1/SortLong.java
@@ -1,29 +1,28 @@
-//package programmers.level_1;
-//
-//import java.util.Arrays;
-//import java.util.Collections;
-//
-//public class SortLong {
-//
-//    public static void main(String[] args) {
-//        solution(873211);
-//    }
-//
-//    public static long solution(long n) {
-//        StringBuilder sb = new StringBuilder();
-//        String tmp = String.valueOf(n);
-//        int[] answerArr = new int[tmp.length()];
-//        for (int i = 0; i < tmp.length(); i++) {
-//            answerArr[i] = Integer.valueOf(tmp.charAt(i));
-//        }
-//
-//        Arrays.sort(answerArr, Collections.reverseOrder());
-//
-//        for (int i = 0; i < answerArr.length; i++) {
-//            sb.append(answerArr[i]);
-//        }
-//
-//        return Long.parseLong(sb.toString());
-//    }
-//
-//}
+package programmers.level_1;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortLong {
+    public static void main(String[] args) {
+        System.out.println(solution(8766321));
+    }
+    public static long solution(long n) {
+        List<String> tmp = new ArrayList<>();
+        String str = String.valueOf(n);
+
+        for (int i = 0; i < str.length(); i++) {
+            tmp.add(String.valueOf(str.charAt(i)));
+        }
+
+        tmp.sort(Comparator.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tmp.size(); i++) {
+            sb.append(tmp.get(i));
+        }
+
+        return Long.parseLong(sb.toString());
+    }
+}
