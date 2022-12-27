8ac2b4d05bd01a3d078137e71933a89c4dad256e
alpacaHyun <flaehdan@gmail.com>
2022-12-27 22:44:20 +0900

    feat : 프로그래머스 Lv1
    body :

---
+++ src/programmers/level_1/SquareStar.java
@@ -0,0 +1,21 @@
+package programmers.level_1;
+
+import java.util.Scanner;
+
+public class SquareStar {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int j = 0; j < b; j++) {
+
+            for (int i = 0; i < a; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
