cfd54ff840316ca8a20c213657287f8651598058
alpacaHyun <flaehdan@gmail.com>
2022-12-28 19:35:11 +0900

    feat : 프로그래머스
    body :

---
+++ src/programmers/level_1/RemoveSmallNumber.java
@@ -0,0 +1,36 @@
+package programmers.level_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveSmallNumber {
+
+
+
+    public static int[] solution(int[] arr) {
+        List<Integer> answerList = new ArrayList<>();
+
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != min) {
+                answerList.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[answerList.size()];
+
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+
+}
