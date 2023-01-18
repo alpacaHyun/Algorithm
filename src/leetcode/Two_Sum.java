98b30770643a12f6f4a0ee2ad0311e61944bcafc
birdieHyun <flaehdan@gmail.com>
2023-01-18 12:49:07 +0900

    feat: leetcode
    body

---
+++ src/leetcode/Two_Sum.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class Two_Sum {
+
+    public int[] solution(int[] nums, int target) {
+        int[] answer = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+
+}
