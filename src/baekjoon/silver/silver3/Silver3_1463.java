bf4f75fb88c083b6280be5911ad7761192d698b9
birdieHyun <flaehdan@gmail.com>
2023-01-21 18:14:58 +0900

    feat: 실버3 DP 숙원사업
    body

---
+++ src/baekjoon/silver/silver3/Silver3_1463.java
@@ -0,0 +1,35 @@
+package baekjoon.silver.silver3;
+
+import java.util.Scanner;
+
+public class Silver3_1463 {
+
+    private static int count = 0;
+
+    public static void main(String[] args) {
+
+        count = 0;
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        while (input != 1) {
+
+            if (input % 6 == 0) {
+
+            } else if (input % 3 == 0){
+
+            } else if (input % 2 == 0) {
+
+            }else {
+
+            }
+
+        }
+
+        System.out.println(count);
+
+
+    }
+
+}
