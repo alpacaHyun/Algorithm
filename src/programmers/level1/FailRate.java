8f389a2dcbc63e2ba002fa4c2e87d7283ff8596a
birdieHyun <flaehdan@gmail.com>
2023-01-26 21:25:05 +0900

    feat: 백준
    body

---
+++ src/programmers/level1/FailRate.java
@@ -0,0 +1,23 @@
+package programmers.level1;
+
+public class FailRate {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+
+
+
+        for (int i = 1; i <= N; i++) {
+            int number = stages[i - 1];
+
+            if (number < i) {
+
+            }
+
+        }
+
+
+
+
+        return answer;
+    }
+}
