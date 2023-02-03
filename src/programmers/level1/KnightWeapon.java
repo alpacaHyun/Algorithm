646e49cc9fecdd10007449551a0a8d7e6ee25392
birdieHyun <flaehdan@gmail.com>
2023-02-03 19:30:06 +0900

    feat: 프로그래머스 Lv1
    body

---
+++ src/programmers/level1/KnightWeapon.java
@@ -0,0 +1,41 @@
+package programmers.level1;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+public class KnightWeapon {
+
+    public static void main(String[] args) {
+        System.out.println(solution(10, 3, 2));
+    }
+
+    public static int solution(int number, int limit, int power) {
+        int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int factors = 0;
+
+            for (int j = 1; j * j <= i; j++) {
+
+                if (j * j == i) {
+                    factors++;
+                } else if (i % j == 0) {
+                    factors += 2;
+                }
+
+
+            }
+
+            if (factors > limit) {
+                answer += power;
+            } else {
+                answer += factors;
+            }
+
+        }
+
+        return answer;
+    }
+}
