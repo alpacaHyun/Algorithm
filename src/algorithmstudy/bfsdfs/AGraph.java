8cf9988724c25a27eab7d870f20355fe0036f865
birdieHyun <flaehdan@gmail.com>
2023-02-05 18:24:59 +0900

    feat: 알고리즘
    body

--- src/algorithmstudy/bfsdfs/AGraph.java
+++ src/algorithmstudy/bfsdfs/AGraph.java
@@ -5,12 +5,10 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class AGraph {
-
     class Node {
         int data;
-        LinkedList<Node> adjacent;
-
         boolean marked;
+        LinkedList<Node> adjacent;
 
         Node(int data) {
             this.data = data;
@@ -21,16 +19,9 @@ public class AGraph {
 
     Node[] nodes;
 
-    AGraph(int size) {
-        nodes = new Node[size];
-        for (int i = 0; i < size; i++) {
-            nodes[i] = new Node(i);
-        }
-    }
-
-    void addEdgd(int i1, int i2) {
-        Node n1 = new Node(i1);
-        Node n2 = new Node(i2);
+    void addEdge(int i1, int i2) {
+        Node n1 = nodes[i1];
+        Node n2 = nodes[i2];
 
         if (!n1.adjacent.contains(n2)) {
             n1.adjacent.add(n2);
@@ -38,31 +29,14 @@ public class AGraph {
         if (!n2.adjacent.contains(n1)) {
             n2.adjacent.add(n1);
         }
-
     }
 
-    void dfs() {
-        dfs(0);
-    }
+    AGraph(int size) {
+        Node[] graph = new Node[size];
 
-    void dfs(int index) {
-        Node root = nodes[index];
-        Stack<Node> stack = new Stack<>();
-        stack.push(root);
-        root.marked = true;
-
-        while (!stack.isEmpty()) {
-            Node r = stack.pop();
-
-            for (Node n : r.adjacent) {
-                if (n.marked == false) {
-                    n.marked = true;
-                    stack.push(n);
-                }
-            }
-            visit(r);
+        for (int i = 0; i < size; i++) {
+            graph[i] = new Node(i);
         }
-
     }
 
     void bfs() {
@@ -70,21 +44,47 @@ public class AGraph {
     }
 
     void bfs(int index) {
-        Node root = new Node(index);
-
+        Node root = nodes[index];
         Queue<Node> queue = new LinkedList();
+
         queue.offer(root);
         root.marked = true;
 
         while (!queue.isEmpty()) {
             Node r = queue.poll();
+
             for (Node n : r.adjacent) {
                 if (n.marked == false) {
                     n.marked = true;
                     queue.offer(n);
                 }
+                visit(n);
             }
-            visit(r);
+
+        }
+    }
+
+    void dfs(){
+        dfs(0);
+    }
+
+    void dfs(int index) {
+        Node root = nodes[index];
+        Stack<Node> stack = new Stack<>();
+
+        stack.push(root);
+        root.marked = true;
+
+        while (!stack.isEmpty()) {
+            Node n = stack.pop();
+
+            for (Node r : n.adjacent) {
+                if (r.marked == false) {
+                    r.marked = true;
+                    stack.push(r);
+                }
+            }
+            visit(n);
         }
     }
 
@@ -95,7 +95,6 @@ public class AGraph {
 
         r.marked = true;
         visit(r);
-
         for (Node n : r.adjacent) {
             if (n.marked == false) {
                 dfsRecursion(n);
@@ -104,6 +103,6 @@ public class AGraph {
     }
 
     void visit(Node node) {
-        System.out.println(node.data + " ");
+        System.out.println(node.data);
     }
 }
---
+++ src/baekjoon/silver/silver2/Silver2_2644.java
@@ -0,0 +1,124 @@
+package baekjoon.silver.silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Silver2_2644 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int people = Integer.parseInt(br.readLine());
+        Graph graph = new Graph(people);
+
+        int answer = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int from = Integer.parseInt(st.nextToken());
+        int to = Integer.parseInt(st.nextToken());
+
+        int cycle = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < cycle; i++) {
+            StringTokenizer tmp = new StringTokenizer(br.readLine(), " ");
+            int i1 = Integer.parseInt(tmp.nextToken());
+            int i2 = Integer.parseInt(tmp.nextToken());
+
+            graph.addEdge(i1, i2);
+        }
+
+        Graph.Node root = graph.nodes[from - 1];
+        int i = graph.dfsRecursion(root, to, answer);
+
+        if (i == 0) {
+            System.out.println(-1);
+        }
+    }
+
+
+    static class Graph {
+        class Node {
+            int data;
+            boolean marked;
+            LinkedList<Node> adjacent;
+
+            Node(int data) {
+                this.data = data;
+                this.marked = false;
+                this.adjacent = new LinkedList<>();
+            }
+        }
+
+        Node[] nodes;
+
+        Graph(int size) {
+            nodes = new Node[size];
+
+            for (int i = 1; i <= size; i++) {
+                nodes[i - 1] = new Node(i);
+            }
+        }
+
+        void addEdge(int i1, int i2) {
+            Node node1 = nodes[i1 - 1];
+            Node node2 = nodes[i2 - 1];
+
+            if (!node1.adjacent.contains(node2)) {
+                node1.adjacent.add(node2);
+            }
+            if (!node2.adjacent.contains(node1)) {
+                node2.adjacent.add(node1);
+            }
+        }
+
+        int dfs(int from, int to) {
+            Node root = nodes[from];
+            root.marked = true;
+            int count = 0;
+
+            Stack<Node> stack = new Stack<>();
+            stack.push(root);
+
+            while (!stack.isEmpty()) {
+                Node pop = stack.pop();
+
+                for (Node n : pop.adjacent) {
+                    if (n.marked == false) {
+                        n.marked = true;
+                        stack.push(n);
+                        count++;
+                        if (n.data == to) {
+                            return count;
+                        }
+                    }
+                }
+                count--;
+            }
+
+            return -1;
+        }
+
+        int dfsRecursion(Node node, int to, int count) {
+            if (node.data == to) {
+                System.out.println(count);
+                count+= 10;
+                return count;
+            }
+            if (node == null) {
+                return count;
+            }
+
+            count++;
+            node.marked = true;
+            for (Node n : node.adjacent) {
+                if (n.marked == false) {
+                    count = dfsRecursion(n, to, count);
+                }
+            }
+            return --count;
+        }
+
+    }
+}
